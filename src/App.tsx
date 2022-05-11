694f807ce8fe8ca5ba811c66ee01efb14aeb5594
Kodai Hagihara <39426616+EringiV3@users.noreply.github.com>
2022-05-11 22:58:04 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Input } from '@chakra-ui/react';
 
 function App() {
-  return <Box>hoge</Box>;
+  return (
+    <Container maxW="container.lg">
+      <Heading textAlign="center">Room example</Heading>
+      <Box
+        display="flex"
+        gap="10px"
+        marginTop="40px"
+        justifyContent="space-between"
+      >
+        <Box>
+          <Box>
+            <video width="400px" autoPlay muted playsInline />
+          </Box>
+          <Box display="flex" alignItems="center" gap="10px">
+            <Input placeholder="room name" />
+            <Button>Join</Button>
+            <Button>Leave</Button>
+          </Box>
+        </Box>
+        <Box>remote rooms</Box>
+        <Box>
+          <Box>messages</Box>
+          <Box display="flex" alignItems="center">
+            <Input />
+            <Button>Send</Button>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
